cb9462a9d1e148504eeca870841f51eb6c6d941f
BanewarSainath <142088224+BanewarSainath@users.noreply.github.com>
2023-08-12 15:37:05 +0530

    Add files via upload

---
+++ testimonial.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <link rel="stylesheet" href="./css/testimonial.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SK construction</title>
+  <link rel="stylesheet" href="/skconstruction/css/home.css">
+  <link rel="stylesheet" href="bootstrap.css">
+  <script type="text/javascript" src="/skconstruction/js/jquery.min.js"></script>
+  <script type="text/javascript" src="/skconstruction/js/bootstrap.js"></script>
+
+</head>
+
+<body>
+  <div class="headbar"><a class="mail" href="mailto:sainathbanewar@gmail.com"> adeshalitkar@gmail.com
+    </a>&nbsp;&nbsp;&nbsp;
+    <a class="call" href="tel:+919022169776">9022169776</a>
+  </div>
+
+  <div class="topbar">
+
+    <a class="navbar-brand " href="home.php"><img class="img-fluid" src="Screenshot (201).png" alt="logo"></a>
+
+
+
+
+
+    <nav class="navbar offset-md-1">
+      <nav class="navbar navbar-expand-lg navbar-light bg-white">
+
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ">
+            <li class="nav-item active">
+              <a class="nav-link mr-md-4" href="http://localhost/skconstruction/home.php">Home <span
+                  class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mr-md-4 " href="http://localhost/skconstruction/about.php">about</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle mr-md-4" href="#" id="navbarDropdown" role="button"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Projects
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="http://localhost/skconstruction/our_projects/residensial/residencial.php">Recidensial</a>
+                <a class="dropdown-item" href="http://localhost/skconstruction/our_projects/commercial/commercial.php">comercial</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="http://localhost/skconstruction/our_projects/indestrial/industrial.php">industrial</a>
+                <a class="dropdown-item" href="http://localhost/skconstruction/our_projects/goverment/gov.php">goverment</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled mr-md-4"
+                href="http://localhost/skconstruction/testimonial.php">Testimonials</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled  mr-md-4" href="http://localhost/skconstruction/contact.php">Contact Us</a>
+            </li>
+          </ul>
+
+        </div>
+        <form class="form-inline my-2 my-lg-0 ml-md-4   mr-md-4">
+
+          <button class="btn btn-outline-success my-2 my-sm-0 " type="submit">Enquiry</button>
+        </form>
+
+      </nav>
+
+    </nav>
+
+  </div>
+  </div>
+
+
+  <div class="profiles container text-center">
+    <h1 class="display-4">Our Testimonials</h1><hr>
+    <div class="row text-center">
+    <div class="col col-md-4  ">
+      <img src="img/swapnilsir.jpg" alt="adeshalitkar" class="  rounded img-fluid img-thumbnail ">
+      <h3>Swapnil Kshirsagar</h3>
+    </div>
+    <!--<div class="col mt-md-5">
+    <h1 class=" text-center display-4 text-danger">SK Construction</h1>
+    </div>-->
+    <div class="col col col-md-4 offset-md-4">
+      <img src="img/adesh.jpg" alt="adeshalitkar" class="  rounded img-fluid img-thumbnail">
+      <h4> Adesh Sanjay Galitkar</h4>
+    </div>
+    </div>
+
+
+
+
+  </div>
+
+</body>
+
+</html>
