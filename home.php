cf41297c0113ea69ee0a2ae14c6addeaa6407126
BanewarSainath <142088224+BanewarSainath@users.noreply.github.com>
2023-08-12 15:33:52 +0530

    Add files via upload
    Homepage

---
+++ home.php
@@ -0,0 +1,432 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SK construction</title>
+  <link rel="stylesheet" href="/skconstruction/css/home.css">
+  <link rel="stylesheet" href="bootstrap.css">
+  <script type="text/javascript" src="/skconstruction/js/jquery.min.js"></script>
+  <script type="text/javascript" src="/skconstruction/js/bootstrap.js"></script>
+
+</head>
+
+<body>
+  <div class="headbar col-sm-12"><a class="mail" href="mailto:sainathbanewar@gmail.com"> adeshalitkar@gmail.com
+    </a>&nbsp;&nbsp;&nbsp;
+    <a class="call" href="tel:+919022169776">9022169776</a>
+  </div>
+
+  <div class="topbar">
+
+    <a class="navbar-brand " href="home.php"><img class="img-fluid" src="Screenshot (201).png" alt="logo"></a>
+
+
+
+
+
+    <nav class="navbar offset-md-1">
+      <nav class="navbar navbar-expand-lg navbar-light bg-white">
+
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ">
+            <li class="nav-item active">
+              <a class="nav-link mr-md-4" href="http://localhost/skconstruction/home.php">Home <span
+                  class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mr-md-4 " href="http://localhost/skconstruction/about.php">about</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle mr-md-4" href="#" id="navbarDropdown" role="button"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Projects
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="http://localhost/skconstruction/our_projects/residensial/residencial.php">Recidensial</a>
+                <a class="dropdown-item" href="http://localhost/skconstruction/our_projects/commercial/commercial.php">comercial</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="http://localhost/skconstruction/our_projects/indestrial/industrial.php">industrial</a>
+                <a class="dropdown-item" href="http://localhost/skconstruction/our_projects/goverment/gov.php">goverment</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled mr-md-4"
+                href="http://localhost/skconstruction/testimonial.php">Testimonials</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled  mr-md-4" href="http://localhost/skconstruction/contact.php">Contact Us</a>
+            </li>
+          </ul>
+
+        </div>
+        <form class="form-inline my-2 my-lg-0 ml-md-4   mr-md-4">
+
+         <!-- <button class="btn btn-outline-success my-2 my-sm-0 " type="submit">Enquiry</button>-->
+         <!-- Button trigger modal -->
+<button type="button" class="btn btn-outline-success my-2 my-sm-0" data-toggle="modal" data-target="#exampleModalCenter">
+ Enquiry
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="exampleModalLongTitle">Enquiry</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="headbar col-sm-12"><h5>Mail us on  :</h5><a class="mail" href="mailto:sainathbanewar@gmail.com"> adeshalitkar@gmail.com
+    </a><br><br>
+    <h5>Call us on  :</h5>
+
+    
+    <a class="call" href="tel:+919022169776">9022169776</a><br><br>
+    <h5>Address :</h5>
+    <h6>SK Construction, opposit to IIMS College, Nere-Dattawadi, Mulshi, Pune-410506
+    </h6>
+
+
+  </div>
+        
+      </div>
+    </div>
+  </div>
+</div>
+        </form>
+
+      </nav>
+
+    </nav>
+
+  </div>
+  </div>
+
+
+
+
+
+
+
+
+
+   <!--<div class="banner ">
+   <div class="slogan container-fluid ">
+      <h1>rESIDENTIAL <br> CONSTRUCTION
+
+      </h1>
+      <hr>
+      <div class="container-fluid col-md-5">
+        <h4>
+          We make your dreams <br> reality
+        </h4>
+
+      </div>
+      <button><a href="more.html"> more details </a></butoon>
+    </div>-->
+
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="http://localhost/pankaj-awandkar-2001-manoj-sonavane.jpg" alt="First slide">
+      <div class="carousel-caption ">
+      <div class="slogan container-fluid ">
+      <a href="http://localhost/skconstruction/our_projects/residensial/residencial.php">
+        <h1 class= "display-3 d-none d-md-block">RESIDENTIAL <br> CONSTRUCTION</h1></a>
+
+        <div class="carousel-caption d-{lg,xl}-none d-md-none d-sm-block ">
+      <div class="slogan container-fluid ">
+      <a href="http://localhost/skconstruction/our_projects/residensial/residencial.php">
+        <h6 class= "">RESIDENTIAL <br> CONSTRUCTION</h6></a>
+      </div>
+      </div>
+      
+      <hr>
+      <div class="container-fluid col-md-5 d-none d-md-block ">
+        <h4 >
+          We make your dreams <br> reality
+        </h4>
+
+      </div>
+      
+      <!--<button><a href="more.html"> RESIDENTIAL </a></butoon>-->
+      
+      </div>
+      </div>
+    
+
+        </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="http://localhost/pankaj-awandkar-2001-manoj-sonavane.jpg" alt="Second slide">
+      <div class="carousel-caption ">
+      <div class="slogan container-fluid ">
+      <h1 class= "display-3 d-none d-md-block">
+        <a href="http://localhost/skconstruction/our_projects/commercial/commercial.php">COMMERCIAL <br> CONSTRUCTION</a>
+        </h1>
+
+      <div class="carousel-caption d-{lg,xl}-none d-md-none d-sm-block ">
+      <div class="slogan container-fluid ">
+      <a href="http://localhost/skconstruction/our_projects/commercial/commercial.php">
+        <h6 class= "">COMMERCIAL <br> CONSTRUCTION</h6></a>
+      </div>
+      </div>
+
+      
+      <hr>
+      <div class="container-fluid col-md-5 d-none d-md-block">
+        <h4 >
+          We make your dreams <br> reality
+        </h4>
+
+      </div>
+      
+      <!--<button><a href="more.html"> COMMERTIAL </a></butoon>-->
+      
+      </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="http://localhost/pankaj-awandkar-2001-manoj-sonavane.jpg" alt="Third slide">
+      <div class="carousel-caption ">
+      <div class="slogan container-fluid ">
+      <h1 class= "display-3 d-none d-md-block"><a href="http://localhost/skconstruction/our_projects/indestrial/industrial.php">INDUSTRIAL <br> CONSTRUCTION</a></h1>
+
+
+      <div class="carousel-caption d-{lg,xl}-none d-md-none d-sm-block ">
+      <div class="slogan container-fluid ">
+      <a href="http://localhost/skconstruction/our_projects/indestrial/industrial.php">
+        <h6 class= "">INDUSTRIAL <br> CONSTRUCTION</h6></a>
+      </div>
+      </div>
+    
+
+      </h1>
+      <hr>
+      <div class="container-fluid col-md-5 d-none d-md-block">
+        <h4 >
+          We make your dreams <br> reality
+        </h4>
+
+      </div>
+      
+      <!--<button><a href="more.html"> INDUSTRIAL</a></butoon>-->
+      
+      </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="http://localhost/pankaj-awandkar-2001-manoj-sonavane.jpg" alt="Third slide">
+      <div class="carousel-caption ">
+      <div class="slogan container-fluid ">
+      <h1 class= "display-3 d-none d-md-block"><a href="http://localhost/skconstruction/our_projects/commercial/commercial.php">GOVERMENT <br> CONSTRUCTION</a>
+
+      </h1>
+      <div class="carousel-caption d-{lg,xl}-none d-md-none d-sm-block ">
+      <div class="slogan container-fluid ">
+      <a href="http://localhost/skconstruction/our_projects/goverment/gov.php">
+        <h6 class= "">GOVERMENT <br> CONSTRUCTION</h6></a>
+      </div>
+      </div>
+      <hr>
+      <div class="container-fluid col-md-5 d-none d-md-block">
+        <h4 >
+          We make your dreams <br> reality
+        </h4>
+
+      </div>
+      
+      <!--<button><a href="more.html"> GOVERMENT </a></butoon>-->
+      
+      </div>
+      </div>
+    </div>
+  </div>
+   <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+  </ol>
+
+
+
+
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
+
+
+
+  
+
+
+
+
+
+  <div class="welcome col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+
+
+    <div>
+
+      <h3>Welocome to</h3>
+      <h1>SK Construction & Design</h1>
+
+    </div>
+    <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We
+      build, The company does not rest on the laurels andcontinues to endeavor towards the future progress and <br>
+      development to reach the peaks of the engineering professionviz . Excellence expediency and economy, QC methods
+      <br>
+      project management and control tools, safety manuals is the hall mark of company’s operation as an organization,
+      <br>
+      it isalways on the move to improve its productivity further by resorting to new management of tools as they become
+      <br>
+      available .<br>
+      &nbsp;&nbsp;&nbsp;&nbsp; We provide a comprehensive range of professional construction services within the
+      residential, commercial, <br>
+      hospitality, educational, infrastructure and industrial sectors in India.</p> <br><br>
+  </div>
+
+
+  <div class="skills ">
+    <h2>Our Expertise</h2>
+  </div>
+  <div class="parenttag row">
+    <div class="tag1 ">
+      <ul>
+        <li>R.C.C. Structure & Designing </li><br>
+        <li> Architecture Plan </li><br>
+        <li> MS Structure & Designing </li>
+      </ul>
+    </div>
+    <div class="tag2 ">
+      <ul>
+        <li>Land Development </li><br>
+        <li> Civil and Infrastructure </li><br>
+        <li> Site survey Planning & Execution of Project</li>
+      </ul>
+    </div>
+    <div class="tag3  ">
+      <ul>
+        <li>Industrial , Commercial & Residential Electric Work </li><br>
+        <li> Bituminous & Concrete Trimix Road </li>
+
+      </ul>
+    </div>
+    <div class="tag4  ">
+      <ul>
+        <li>PEB Industrial Shed & Designing</li><br>
+        <li> Mivan Shuttering Work </li><br>
+        <li> Valuation & Estimating </li>
+      </ul>
+    </div>
+  </div>
+
+  </div>
+  <!--<div id="goal container">
+        <h2>Our Goals</h2>
+        <div class="parentg row">
+        
+        <div class="g1 col-lg-4 col-md-4  "> <h4>Hkfl-Quality Build</h4>  
+        <p>
+            </p>
+        </div>
+              <div class="g2 col-lg-4 col-md-4 "> <h4>High-Quality Build</h4>  
+                <p>We try to run our sites as efficient as factories
+                     – no wastages, no mistakes, max quality.
+                      We perform 1000+ Quality Checks on 
+                      every project.
+                    </p>
+                </div>
+            </div>
+         </div>-->
+  <div class=" container-fluid card-deck">
+    
+    <h1>
+      What <br> We Do</h1>
+
+
+   
+
+      <div class="residencial card col-md-3">
+        <img class="card-img-top" src="http://localhost/skconstruction/img/h12.jpeg" alt="residential">
+        <div class="card-footer">
+
+          <h4 class="card-title bg-light">RESIDENTIAL</h4>
+
+        </div>
+
+      </div>
+      <div class="comercial card">
+      <img class="card-img-top" src="http://localhost/skconstruction/img/h4.png" alt="commercial">
+        <div class="card-footer">
+          <h4 class="card-title bg-light">COMMERCIAL</h4>
+
+        </div>
+      </div>
+      <div class="indestiarl card ">
+      <img class="card-img-top " src="http://localhost/skconstruction/img/architect-3979490__480.jpg" alt="industrial">
+        <div class="card-footer">
+          <h4 class="card-title bg-light">INDUSTRIAL</h4>
+
+        </div>
+
+      </div>
+      <div class="goverment card ">
+      <img class="card-img-top" src="http://localhost/skconstruction/img/crane-homes-1576103__480.jpg" alt="gov">
+        <div class="card-footer">
+          <h4 class="card-title bg-light">GOVERNMENTAL</h4>
+        </div>
+      </div>
+    </div>
+  
+
+
+
+  <div id="feedback">
+    <h4> Client's Feedback</h4>
+    <!--write something-->
+  </div>
+
+  <footer>
+    <div>&copy; Copyright SK construction &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
+      &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
+      All rights are reserved</div>
+  </footer>
+
+
+
+
+
+
+</body>
+
+</html>
